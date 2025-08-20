70480c6d492f3dcabddfe6c1c2ab5c476784cfdd
stan <stan@mail.coinsdo.com>
2025-08-20 10:16:37 +0800

    feat(stan)：定时任务bug修复

--- src/main/java/com/notice/daily/briefings/NoticeDailyBriefingsApplication.java
+++ src/main/java/com/notice/daily/briefings/NoticeDailyBriefingsApplication.java
@@ -4,9 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.notice.daily.briefings.mapper")
+@EnableScheduling
 public class NoticeDailyBriefingsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/notice/daily/briefings/notice/feishu/SendMsg.java
+++ src/main/java/com/notice/daily/briefings/notice/feishu/SendMsg.java
@@ -3,7 +3,6 @@ package com.notice.daily.briefings.notice.feishu;
 import com.alibaba.fastjson2.JSONObject;
 import com.notice.daily.briefings.service.HttpService;
 import lombok.extern.slf4j.Slf4j;
-import okhttp3.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -19,16 +18,11 @@ public class SendMsg {
 
     public void send(String msg) {
         String url = feishuRobotLink;
-//        String json = "{'msg_type':'text','content':{'text':'" + msg + "'}}";
-
         JSONObject jsonObject = new JSONObject();
-
         jsonObject.put("msg_type", "text");
         JSONObject content = new JSONObject();
         content.put("text", msg);
         jsonObject.put("content", JSONObject.toJSONString(content));
-
-
         try {
             httpService.postJson(url, jsonObject.toJSONString());
         } catch (Exception e) {
