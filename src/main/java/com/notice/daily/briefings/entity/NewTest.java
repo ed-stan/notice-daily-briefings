83521daf0534a7b2e4cc6e134920b40a231ea001
stan <stan@mail.coinsdo.com>
2025-10-02 17:57:15 +0800

    feat(stan)：第一次修改
    feat(stan)：第二次修改
    ----------------将第二次修改合并到第一次修改
    
    feat(stan)：第三次修改
    ----------------将第三次修改合并到第一次修改

---
+++ src/main/java/com/notice/daily/briefings/entity/NewTest.java
@@ -0,0 +1,11 @@
+package com.notice.daily.briefings.entity;
+
+public class NewTest {
+
+    public static void test() {
+        System.out.println("hello world");
+    }
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
