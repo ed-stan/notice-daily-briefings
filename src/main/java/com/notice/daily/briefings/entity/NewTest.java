bc7a82d6beba1dc1a080e0c606dbcb2785018aa0
stan <stan@mail.coinsdo.com>
2025-10-02 17:57:47 +0800

    feat(stan)：第三次修改

--- src/main/java/com/notice/daily/briefings/entity/NewTest.java
+++ src/main/java/com/notice/daily/briefings/entity/NewTest.java
@@ -1,6 +1,10 @@
 package com.notice.daily.briefings.entity;
 
 public class NewTest {
+
+    public static void test() {
+        System.out.println("hello world");
+    }
     public static void main(String[] args) {
         System.out.println("hello world");
     }
